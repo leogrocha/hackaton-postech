e8f8ebe9304f9736c52cc5755a9166934f46ccf8
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2024-03-12 19:33:03 -0300

    feat: fist commit

---
+++ src/main/java/com/hackaton/postech/PostechApplication.java
@@ -0,0 +1,13 @@
+package com.hackaton.postech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostechApplication.class, args);
+	}
+
+}
